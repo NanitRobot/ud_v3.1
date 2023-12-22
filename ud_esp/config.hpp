4bc39367d9c7ca4aa08f8966fff7089995f5b77a
ItVergil251 <ivanaskad@gmail.com>
2023-12-22 15:21:48 +0200

    deleted full shit and created configs

---
+++ ud_esp/config.hpp
@@ -0,0 +1,58 @@
+#include <NanitLib.h>
+#include <DHT.h> 
+#include <TM1637Display.h>
+#include <Keypad.h>  
+#include <Password.h>
+
+#define SERVO_PIN P1_1 
+//MOTOR1_A вже готовий дефайн P1_3 
+//MOTOR1_B вже готовий дефайн P1_4
+
+#define IN1       P2_1
+#define IN2       P2_2
+#define IN3       P2_3
+#define IN4       P2_4
+
+#define TL_RED    P3_2
+#define TL_YELLOW P3_3
+#define TL_GREEN  P3_4
+
+#define RGB_RED   P4_2
+#define RGB_GREEN P4_3
+#define RGB_BLUE  P4_4
+
+#define PIR_PIN   P5_1
+#define SOUND_PIN P5_2
+#define BUZ_PIN   P5_3
+#define LINE_PIN  P5_4
+
+#define DHT11_PIN P6_1
+#define MQ7_PIN   P6_2
+#define LIGHT_PIN P6_3
+
+#define C4        P10_4
+#define C3        P10_3
+#define C2        P10_2
+#define C1        P10_1
+
+#define R1        P7_4
+#define R2        P7_3
+#define R3        P7_2
+#define R4        P7_1
+
+#define TRIG_PIN  P9_3
+#define ECHO_PIN  P9_4 
+#define CLK       P9_1
+#define DIO       P9_2
+
+#define wifi_esp 1      //Якщо в розумному будинку використовується WiFi то має бути розкоментовано якщо ні - закоментуйте
+// #define bluetooth 1  //Якщо в розумному будинку використовується BlTh то має бути розкоментовано якщо ні - закоментуйте
+
+void port_1_init();
+void port_2_init();
+void port_3_init();
+void port_4_init();
+void port_5_init();
+void port_6_init(); 
+void port_9_init();
+
