4b3457be68757a04fc33326141604fbd078614d4
ItVergil251 <ivanaskad@gmail.com>
2023-12-26 16:27:21 +0200

    add tested funcs

--- ud_esp/ud_esp.ino
+++ ud_esp/ud_esp.ino
@@ -1,10 +1,24 @@
 #include "config.hpp"
 
+bool flag =0;
 
-
-void setup() {
+void setup() 
+{
+  initdisplay();
+  port_1_init();
+  port_2_init();
+  port_3_init();
+  port_4_init();
+  port_5_init();
+  port_6_init();
+  port_9_init();
 }
 
+void loop() 
+{
+  displaySensors();
+  C_O_filter();
+  window();
+  parkin();
 
-void loop() {
 }
