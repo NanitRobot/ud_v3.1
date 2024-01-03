911845fbcf7062341a39dc5d5097e15b6bffd889
ItVergil251 <ivanaskad@gmail.com>
2024-01-03 18:14:18 +0200

    finished keypass patterns

--- ud_esp/config.cpp
+++ ud_esp/config.cpp
@@ -5,7 +5,7 @@ Servo servo;
 DHT dht(DHT11_PIN, DHT11);
 TM1637Display tm(CLK, DIO);
 
-const byte step_pin[4]{IN1,IN2,IN3,IN4};
+const byte step_pin[4]{ IN1, IN2, IN3, IN4 };
 
 bool
   parkin_flag = 0,
@@ -31,50 +31,50 @@ float
 #ifdef LIGHT_4pin
 uint16_t light_start = 0;
 #endif
-uint16_t  background_screen = 0;
+uint16_t background_screen = 0;
 
-const uint8_t word_stop[] ={
-  SEG_A | SEG_C | SEG_D | SEG_F | SEG_G,            // S
-  SEG_D | SEG_E | SEG_F | SEG_G,                    // t
-  SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,    // O
-  SEG_A | SEG_B | SEG_E | SEG_F | SEG_G             // P
+const uint8_t word_stop[] = {
+  SEG_A | SEG_C | SEG_D | SEG_F | SEG_G,          // S
+  SEG_D | SEG_E | SEG_F | SEG_G,                  // t
+  SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,  // O
+  SEG_A | SEG_B | SEG_E | SEG_F | SEG_G           // P
 };
 
-const uint8_t word_wait[] ={
-  SEG_B | SEG_D | SEG_F | SEG_G,                    // W
-  SEG_A | SEG_B | SEG_C | SEG_E | SEG_F | SEG_G,    // A
-  SEG_B | SEG_C,                                    // I
-  SEG_D | SEG_E | SEG_F | SEG_G,                    // t
+const uint8_t word_wait[] = {
+  SEG_B | SEG_D | SEG_F | SEG_G,                  // W
+  SEG_A | SEG_B | SEG_C | SEG_E | SEG_F | SEG_G,  // A
+  SEG_B | SEG_C,                                  // I
+  SEG_D | SEG_E | SEG_F | SEG_G,                  // t
 };
 
-const uint8_t word_open[] ={
-  SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,    // O
-  SEG_A | SEG_B | SEG_E | SEG_F | SEG_G,            // P
-  SEG_A | SEG_D | SEG_E | SEG_F | SEG_G,            // E
-  SEG_C | SEG_E | SEG_G,                            // n
+const uint8_t word_open[] = {
+  SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,  // O
+  SEG_A | SEG_B | SEG_E | SEG_F | SEG_G,          // P
+  SEG_A | SEG_D | SEG_E | SEG_F | SEG_G,          // E
+  SEG_C | SEG_E | SEG_G,                          // n
 };
 
 const uint8_t seg_bloc[] = {
-  SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F | SEG_G, // B
-  SEG_E | SEG_D | SEG_F,                                 // L
-  SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,         // O
-  SEG_A | SEG_D | SEG_E | SEG_F,                         // C
- };
+  SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F | SEG_G,  // B
+  SEG_E | SEG_D | SEG_F,                                  // L
+  SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,          // O
+  SEG_A | SEG_D | SEG_E | SEG_F,                          // C
+};
 
-const byte ROWS = 4; // Кількість рядків у клавіатурі (Number of rows in the keypad)
-const byte COLS = 4; // Кількість стовпців у клавіатурі (Number of columns in the keypad)
+const byte ROWS = 4;  // Кількість рядків у клавіатурі (Number of rows in the keypad)
+const byte COLS = 4;  // Кількість стовпців у клавіатурі (Number of columns in the keypad)
 // Масив символів для клавіш(Array of characters)
 char hexaKeys[ROWS][COLS] = {
-  {'1','2','3','A'},
-  {'4','5','6','B'},
-  {'7','8','9','C'},
-  {'*','0','#','D'}
+  { '1', '2', '3', 'A' },
+  { '4', '5', '6', 'B' },
+  { '7', '8', '9', 'C' },
+  { '*', '0', '#', 'D' }
 };
 // Масив пінів для рядків та стовпців (Array of pins for rows and columns)
-byte rowPins[ROWS] = {R1, R2, R3, R4};
-byte colPins[COLS] = {C1, C2, C3, C4};
+byte rowPins[ROWS] = { R1, R2, R3, R4 };
+byte colPins[COLS] = { C1, C2, C3, C4 };
 // Створення об'єкту класу Keypad(Creating a Keypad class object)
-Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
+Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
 Password password = Password("123A");
 String newPassword;  // зберігає новий пароль (holds the new password)
 char passchars[6];   // символи для newPassword (characters of newPassword)
@@ -82,11 +82,9 @@ byte maxPasswordLength = 6;
 byte currentPasswordLength = 0;
 
 
-void keypad(void)
-{
-  char Key = customKeypad.getKey(); // Зчитування натиснутої клавіші (Reading the pressed key)
-  if (Key)
-  {
+void keypad(void) {
+  char Key = customKeypad.getKey();  // Зчитування натиснутої клавіші (Reading the pressed key)
+  if (Key) {
     keytap_buz();
     switch (Key) {
       case '*': resetPassword(); break;
@@ -94,49 +92,80 @@ void keypad(void)
       default: processNumberKey(Key);
     }
     rgb4_set(0, 0, 1);
-  delay(50);
+    delay(50);
   }
 }
 
-void processNumberKey(char key) 
-{
+void processNumberKey(char key) {
   currentPasswordLength++;
   password.append(key);
-  if (currentPasswordLength == maxPasswordLength) 
-  {
+  if (currentPasswordLength == maxPasswordLength) {
     // Якщо довжина пароля більше 6, то виконати перевірку (If password length is more than 6, perform password check)
-    if (checkPassword()){for (short i = 0; i<3; i++){access_grant_buz();}}
-    else {for (short i = 0; i<10; i++){access_denid_buz();}} 
+    checkPassword();
   }
 }
 
-bool checkPassword(void) 
-{
-  if (password.evaluate()) 
-  {
-    tft.fillScreen(ST7735_GREEN);
-    lock_flag = 1; // Вимкнення режиму введення пароля (Turn off password entry mode)
-    tft.fillScreen(background_screen);
-    return true;
-    rgb4_set(0, 1, 0);
+void checkPassword(void) {
+  if (password.evaluate()) {
+    for (byte i = 0; i <= 6; i++) {
+      rgb4_set(1, 1, 0);
+      access_grant_buz();
+      delay(25);
+      rgb4_set(0, 0, 0);
+      access_grant_buz();
+    }
+    lock_flag = !lock_flag;
+    if (lock_flag) {
+      tft.fillScreen(background_screen);
+      tft.setTextColor(~background_screen);
+    } else {
+      tm.clear();
+  tm.setSegments(seg_bloc);
+      tft.fillScreen(ST7735_RED);
+      tft.setTextColor(ST7735_WHITE);
+    }
   } 
   else 
   {
     tft.fillScreen(ST7735_RED);
+    tft.setCursor(25, 20);
+    tft.setTextSize(4);
+    tft.setTextColor(ST7735_WHITE);
+    tft.print("WRONG");
+    tft.setCursor(35, 50);
+    tft.print("PASS");
+    for (byte i = 0; i <= 5; i++) {
+      rgb4_set(1, 0, 1);
+      access_denid_buz();
+      delay(10);
+      rgb4_set(1, 0, 0);
+      access_denid_buz();
+      delay(10);
+      rgb4_set(0, 0, 0);
+      access_denid_buz();
+      delay(10);
+      rgb4_set(1, 0, 0);
+      access_denid_buz();
+      delay(10);
+      rgb4_set(1, 0, 1);
+      access_denid_buz();
+      delay(10);
+      tm.clear();
+      tm.setSegments(seg_bloc);
+    }
     lock_flag = 0;
-    return false;
-    rgb4_set(1, 0, 0);
   }
   resetPassword();
 }
 
-void resetPassword() 
-{
-  password.reset(); // Скидання пароля (Reset password)
+void resetPassword() {
+  password.reset();  // Скидання пароля (Reset password)
   currentPasswordLength = 0;
 }
 
-bool locck(void){return lock_flag;}
+bool locck(void) {
+  return lock_flag;
+}
 
 void port_1_init(void) {
   servo.attach(SERVO_PIN);
@@ -150,25 +179,24 @@ void port_1_init(void) {
 }
 
 void port_2_init(void) {
-  for(short k =0; k<4; k++){
-    pinMode(step_pin[k],OUTPUT);
+  for (short k = 0; k < 4; k++) {
+    pinMode(step_pin[k], OUTPUT);
     digitalWrite(step_pin[k], 0);
   }
 }
 
 void port_3_init(void) {
-byte pin_port[3] = {0};
+  byte pin_port[3] = { 0 };
 #ifdef separate_leds3
-pin_port[0] = TL_RED;
-pin_port[1] = TL_YELLOW;
-pin_port[2] = TL_GREEN;
+  pin_port[0] = TL_RED;
+  pin_port[1] = TL_YELLOW;
+  pin_port[2] = TL_GREEN;
 #elif defined(rgb_led3)
-pin_port[0] = RGB_RDL;
-pin_port[1] = RGB_GRN;
-pin_port[2] = RGB_BLU;
+  pin_port[0] = RGB_RDL;
+  pin_port[1] = RGB_GRN;
+  pin_port[2] = RGB_BLU;
 #endif
-  for (short i = 0; i<3; i++) 
-  {
+  for (short i = 0; i < 3; i++) {
     pinMode(pin_port[i], OUTPUT);
     digitalWrite(pin_port[i], 0);
   }
@@ -191,37 +219,29 @@ void port_5_init(void) {
 }
 
 #ifdef active_buz
-void alert_buz(void)
-{
-  for (int i=0; i<600; i++) 
-  {
+void alert_buz(void) {
+  for (int i = 0; i < 600; i++) {
     digitalWrite(BUZ_PIN, !digitalRead(A12));
     delayMicroseconds(400);
   }
 }
 
-void keytap_buz(void)
-{
-  for (int i=0; i<400; i++) 
-  {     
+void keytap_buz(void) {
+  for (int i = 0; i < 400; i++) {
     digitalWrite(BUZ_PIN, !digitalRead(A12));
     delayMicroseconds(100);
   }
 }
 
-void access_grant_buz(void)
-{
-  for (int i=0; i<2000; i++) 
-  {     
+void access_grant_buz(void) {
+  for (int i = 0; i < 2000; i++) {
     digitalWrite(BUZ_PIN, !digitalRead(A12));
     delayMicroseconds(75);
   }
 }
 
-void access_denid_buz(void)
-{
-  for (int i=0; i<700; i++) 
-  {
+void access_denid_buz(void) {
+  for (int i = 0; i < 700; i++) {
     digitalWrite(BUZ_PIN, !digitalRead(A12));
     delayMicroseconds(350);
   }
@@ -263,6 +283,7 @@ void initdisplay(void) {
 }
 
 void displaySensors(void) {
+  tft.setTextSize(2);
   tft.setCursor(10, 10);
   last_gas = analogRead(MQ7_PIN);
   tft.print("CO2: ");
@@ -307,10 +328,10 @@ void displaySensors(void) {
   }
   if (digitalRead(LIGHT_PIN) != light) {
     light = digitalRead(LIGHT_PIN);
-    background_screen =~ background_screen;
+    background_screen = ~background_screen;
     tft.setTextColor(~background_screen);
     tft.fillScreen(background_screen);
-  } 
+  }
 }
 
 void C_O_filter(void) {
@@ -322,9 +343,9 @@ void C_O_filter(void) {
 }
 
 void window(void) {
-  if(!digitalRead(LIGHT_PIN)){window_flag = 1;}
-  if(!digitalRead(LINE_PIN)){window_flag = 0;}
-  servo.write((window_flag)?90:0);
+  if (!digitalRead(LIGHT_PIN)) { window_flag = 1; }
+  if (!digitalRead(LINE_PIN)) { window_flag = 0; }
+  servo.write((window_flag) ? 90 : 0);
 }
 
 void traffic_light(bool r_led, bool y_led, bool g_led) {
@@ -333,39 +354,35 @@ void traffic_light(bool r_led, bool y_led, bool g_led) {
   digitalWrite(TL_GREEN, g_led);
 }
 
-void rgb4_set(bool r_led, bool g_led, bool b_led)
-{
+void rgb4_set(bool r_led, bool g_led, bool b_led) {
   digitalWrite(RGB_RED, r_led);
   digitalWrite(RGB_GREEN, g_led);
   digitalWrite(RGB_BLUE, b_led);
 }
 
-void step_forward(void)
-{
-  for(byte k = 0; k < 128; k++)
-  {
-    for(short i = 0; i<=3; i++)
-    {
+void step_forward(void) {
+  for (byte k = 0; k < 128; k++) {
+    for (short i = 0; i <= 3; i++) {
       digitalWrite(step_pin[i], 1);
       delay(3);
-      digitalWrite(step_pin[i],0);
+      digitalWrite(step_pin[i], 0);
     }
   }
 }
 
-void step_backward(void){
-  for(byte k = 0; k < 128; k++)
-  {
-    for(short i = 3; i>=0; i--)
-    {
+void step_backward(void) {
+  for (byte k = 0; k < 128; k++) {
+    for (short i = 3; i >= 0; i--) {
       digitalWrite(step_pin[i], 1);
       delay(3);
-      digitalWrite(step_pin[i],0);
+      digitalWrite(step_pin[i], 0);
     }
   }
 }
 
-void step_lock(void){for(short k =0; k<4; k++){digitalWrite(step_pin[k], 0);}}
+void step_lock(void) {
+  for (short k = 0; k < 4; k++) { digitalWrite(step_pin[k], 0); }
+}
 
 void parkin(void) {
   digitalWrite(TRIG_PIN, 1);
@@ -394,15 +411,17 @@ void parkin(void) {
   }
 }
 
-void lock_home(void)
-{
+void lock_home(void) {
   servo.write(0);
-  tm.clear();
   tm.setSegments(seg_bloc);
   step_lock();
-  if (digitalRead(PIR_PIN) || digitalRead(SOUND_PIN)) {
-    alert_buz();
+  if (digitalRead(PIR_PIN) || !digitalRead(SOUND_PIN) || !digitalRead(LINE_PIN)) {
+    for (byte i = 0; i < 2; i++) { alert_buz(); }
+    tft.setCursor(10, 40);
+    tft.setTextSize(3);
+    tft.setTextColor(ST7735_WHITE);
+    tft.print("ALARM!!!");
   }
+  delay(100);
   tft.fillScreen(ST7735_RED);
-  rgb4_set(1, 0, 0);
 }
