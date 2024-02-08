8a36005e036361a7a890add758a07e8f6167b02a
ItVergil251 <103885223+ItVergil251@users.noreply.github.com>
2024-02-08 16:56:04 +0200

    commet code

--- ud_esp/config.hpp
+++ ud_esp/config.hpp
@@ -4,37 +4,39 @@
 #include <Keypad.h>  
 #include <Password.h>
 
-#define SERVO_PIN P1_1 
-//MOTOR1_A вже готовий дефайн P1_3 
-//MOTOR1_B вже готовий дефайн P1_4
+#define SERVO_PIN P1_1 // Макрос піна для серводвигуна (Macro for the Servo Motor Pin)
+//MOTOR1_A готовий макрос піна P1_4 для двигуна ПС (Predefined Macro for Pin P1_4 for a Motor DC) 
+//MOTOR1_B готовий макрос піна P1_3 для двигуна ПС (Predefined Macro for Pin P1_3 for a Motor DC)
 
-#define IN1       P2_1    // Пін 1 для крокового двигуна
-#define IN2       P2_2    // Пін 2 для крокового двигуна
-#define IN3       P2_3    // Пін 3 для крокового двигуна
-#define IN4       P2_4    // Пін 4 для крокового двигуна
+#define IN1 P2_1    // Макрос піна 1 для крокового двигуна (Macro for Pin 1 of the Stepper Motor)
+#define IN2 P2_2    // Макрос піна 2 для крокового двигуна (Macro for Pin 2 of the Stepper Motor)
+#define IN3 P2_3    // Макрос піна 3 для крокового двигуна (Macro for Pin 3 of the Stepper Motor)
+#define IN4 P2_4    // Макрос піна 4 для крокового двигуна (Macro for Pin 4 of the Stepper Motor)
 
-// #define RGB_LED_3 1     // Макрос для RGB світлофора (Macro for RGB Traffic Light)
-#define SEPARATE_LEDS_3 1  // Макрос для звичайного світлофора (Macro for Standard Traffic Light)
+// #define RGB_LED_3 1    // Макрос для RGB світлофора (Macro for RGB Traffic Light)
+#define SEPARATE_LEDS_3 1 // Макрос для звичайного світлофора (Macro for Standard Traffic Light)
 
-#if defined(SEPARATE_LEDS_3)     // Піни звичайного світлофора (Pins of a Standard Traffic Light)
+#if defined(SEPARATE_LEDS_3) // Піни звичайного світлофора (Pins of a Standard Traffic Light)
 
-#define TL_RED P3_2          // Пін червоного світлодіода для світлофора (Pin for the Red LED of the Traffic Light)
-#define TL_YLW P3_3          // Пін жовтого світлодіода для світлофора (Pin for the Yellow LED of the Traffic Light)
-#define TL_GRN P3_4          // Пін зеленого світлодіода для світлофора (Pin for the Green LED of the Traffic Light)
+#define TL_RED P3_2 // Пін червоного світлодіода для світлофора (Pin for the Red LED of the Traffic Light)
+#define TL_YLW P3_3 // Пін жовтого світлодіода для світлофора (Pin for the Yellow LED of the Traffic Light)
+#define TL_GRN P3_4 // Пін зеленого світлодіода для світлофора (Pin for the Green LED of the Traffic Light)
 
-#elif defined(RGB_LED_3)         // Піни RGB світлофора (Pins of an RGB Traffic Light)
+#elif defined(RGB_LED_3) // Піни RGB світлофора (Pins of an RGB Traffic Light)
 
-#define RGB_RDL P3_2            // Пін червоного кольору RGB світлофора (Pin for the Red Color of the RGB Traffic Light)
-#define RGB_GRN P3_3            // Пін зеленого кольору RGB світлофора  (Pin for the Green Color of the RGB Traffic Light)
-#define RGB_BLU P3_4            // Пін синього кольору RGB світлофора   (Pin for the Blue Color of the RGB Traffic Light)
-
+#define RGB_RDL P3_2 // Пін червоного кольору RGB світлофора (Pin for the Red Color of the RGB Traffic Light)
+#define RGB_GRN P3_3 // Пін зеленого кольору RGB світлофора  (Pin for the Green Color of the RGB Traffic Light)
+#define RGB_BLU P3_4 // Пін синього кольору RGB світлофора   (Pin for the Blue Color of the RGB Traffic Light)
 #endif
 
-#define RED 1    // Макрос червоного кольору для функції світлофора (Macro for the Red Color in the Traffic Light Function)
-#define YELLOW 2 // Макрос жовтого кольору для функції світлофора (Macro for the Yellow Color in the Traffic Light Function)
-#define GREEN 3  // Макрос зеленого кольору для функції світлофора (Macro for the Green Color in the Traffic Light Function)
+// Макрос червоного кольору для функції світлофора (Macro for the Red Color in the Traffic Light Function)
+#define RED 1
+// Макрос жовтого кольору для функції світлофора (Macro for the Yellow Color in the Traffic Light Function)
+#define YELLOW 2
+// Макрос зеленого кольору для функції світлофора (Macro for the Green Color in the Traffic Light Function)
+#define GREEN 3
 
-#define OFF 0
+#define OFF 0    // Макрос для вимикання RGB світлодіода (Macro for Turning Off the RGB LED)
 #define RED 1    // Макрос червоного кольору для RGB світлодіода з 4 порта (Macro for Red Color of the RGB LED on Port 4)
 #define GREEN 2  // Макрос зеленого кольору для RGB світлодіода з 4 порта (Macro for Green Color of the RGB LED on Port 4)
 #define BLUE 3   // Макрос синього кольору для RGB світлодіода з 4 порта (Macro for Blue Color of the RGB LED on Port 4)
@@ -55,10 +57,10 @@
 #define ACTIVE_BUZZER 1
 // #define PASSIVE_BUZZER 1
 
-#define ALERT 1
-#define KEYTAP 2 
-#define ACS_DNID 3
-#define ACS_GRNT 4
+#define ALERT 0 // Макрос звуку тривоги для базера (Alert Sound Macro for the Buzzer)
+#define KEYTAP 1 // Макрос звуку клавіатури для базера (Macro for Keyboard Sound for the Buzzer)    
+#define ACS_DNID 2 // Макрос звуку "доступ заборонено" для базера (Sound Macro 'Access Denied' for the Buzzer)
+#define ACS_GRNT 3 // Макрос звуку "доступ дозволено" для базера (Sound Macro 'Access Granted' for the Buzzer)
 
 #define DHT11_PIN P6_1    // Цифровий пін для DHT 11 (Digital Pin for DHT11 Sensor)
 #define MQ7_PIN   P6_2    // Аналоговий пін для датчика газу (Analog Pin for Gas Sensor)
@@ -66,36 +68,23 @@
 
 // #define LIGHT_4pin 1
 
-#define C4        P10_4
-#define C3        P10_3
-#define C2        P10_2
-#define C1        P10_1
+#define R1 P7_4    // Пін для строчки 1 клавіатури (Pin for Row 1 of the Keypad)
+#define R2 P7_3    // Пін для строчки 2 клавіатури (Pin for Row 2 of the Keypad)
+#define R3 P7_2    // Пін для строчки 3 клавіатури (Pin for Row 3 of the Keypad)
+#define R4 P7_1    // Пін для строчки 4 клавіатури (Pin for Row 4 of the Keypad)
 
-#define R1        P7_4
-#define R2        P7_3
-#define R3        P7_2
-#define R4        P7_1
+#define C1 P10_1   // Пін для стовпчика 1 клавіатури (Pin for Column 1 of the Keypad)
+#define C2 P10_2   // Пін для стовпчика 2 клавіатури (Pin for Column 2 of the Keypad)
+#define C3 P10_3   // Пін для стовпчика 3 клавіатури (Pin for Column 3 of the Keypad)
+#define C4 P10_4   // Пін для стовпчика 4 клавіатури (Pin for Column 4 of the Keypad)
 
-#define TRIG_PIN  P9_3
-#define ECHO_PIN  P9_4 
-#define CLK       P9_1
-#define DIO       P9_2
+#define CLK       P9_1  // Пін CLK для драйвера TM1637 (CLK Pin for TM1637 Driver)
+#define DIO       P9_2  // Пін DIO для драйвера TM1637 (DIO Pin for TM1637 Driver)
+#define TRIG_PIN  P9_3  // Пін TRIG для датчика відстані HC-SR04 (TRIG Pin for HC-SR04 Distance Sensor)
+#define ECHO_PIN  P9_4  // Пін ECHO для датчика відстані HC-SR04 (ECHO Pin for HC-SR04 Distance Sensor)
 
-#if !defined(MEGACORE)
-/** @todo  Замінити в коді макровизначення wifi_esp та bluetooth на
- * NANIT_USE_WIFI та NANIT_USE_BlTH відповідно
- *
- * Якщо код використовуватиметься з набором плат Nanit вибір варіанту коду буде
- * доступним через відповідні пункти меню. В інакшому разі потрібно
- * розкоментовувати одне з макровизнечень
- */
-// #define NANIT_USE_WIFI ///<  Якщо в розумному будинку використовується WiFi то має бути розкоментовано якщо ні - закоментуйте
-// #define NANIT_USE_BlTH ///<  Якщо в розумному будинку використовується BlTh то має бути розкоментовано якщо ні - закоментуйте
-#endif
-
-#if defined(NANIT_USE_WIFI) && defined(NANIT_USE_BlTH)
-#error  Вибір одночасно двох варіацій програми поки не передбачено
-#endif
+#define ESP_CONTROL 1 //Якщо в розумному будинку використовується WiFi то має бути розкоментовано якщо ні - закоментуйте
+// #define BT_CONTROL 1  //Якщо в розумному будинку використовується BlTh то має бути розкоментовано якщо ні - закоментуйте
 
 void port_1_init(void);
 void port_2_init(void);
@@ -112,7 +101,7 @@ void rgb4_set(uint8_t color);
 void initdisplay(void);
 
 void displaySensors(void);
-void C_O_filter(void);
+void AirQuality_Fan(void);
 void window(void);
 void parkin(void);
 
